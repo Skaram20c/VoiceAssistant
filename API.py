099c3b2207bb13f331e3e41cf5f10a6dd48c1e55
skaram20c <skaram20c@gmail.com>
2023-04-25 01:36:29 -0400

    added code for Voice Assistant Buddy

---
+++ API.py
@@ -0,0 +1,20 @@
+# I am not adding this file in gitignore as I need this all data in raspberrypi to make it work I will reset all api
+# after semester ends.
+# Meross Info
+meros_email = "##################@gmail.com"
+meros_password = "######################@Rs"
+# Google Map Key
+map_key = '################################'
+# News Api
+news_api = '###############################'
+# Recipe api key
+RECIPE_APP_ID = "##########################"
+RECIPE_APP_KEY = "#########################"
+#Twilio Account
+tw_account_sid = '#########################'
+tw_auth_token = '##########################'
+# Weather Api
+weather_api = '############################'
+# Wolframalpha
+wolf_appId = '#############################'
+openai_api_key = "#########################"
